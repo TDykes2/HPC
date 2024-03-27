eb258cea53ea6165a7ec1e88e5bed9e01b5fd95e
Tyler <tylerdykes86@gmail.com>
2024-03-27 16:45:45 -0400

    update1.2

--- profile.R
+++ profile.R
@@ -1,19 +1,18 @@
 Rprof()
-x_pr = prcomp(
-games <- c(),
+games <- numeric(100000)
 for(i in 1:100000) {
   doors <- c("A", "B", "C")
   ourdoor <- sample(doors, size = 1, replace = FALSE)
   cardoor <- sample(doors, size = 1, replace = FALSE)
   
-  switched <- sample(doors[-which(ourdoor != doors & cardoor!=doors)], size = 1)
+  switched <- sample(doors[!(ourdoor == doors | cardoor == doors)], size = 1)
   
   if(switched == cardoor) {
     games[i] <- 1
-  } else {games[i] <- 0}
-
-},
-mean(games == 1),
-)
+  } else {
+    games[i] <- 0
+  }
+}
+x_pr <- prcomp(games)
 Rprof(NULL)
 summaryRprof()
